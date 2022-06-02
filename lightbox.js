7aad991f27e839ee24e17407f0d70c3e5db44956
nickfff-dev <hunchographics@gmail.com>
2022-06-02 18:58:44 +0300

    lightbox with vanilla js

---
+++ lightbox.js
@@ -0,0 +1,40 @@
+
+const imagedata = [
+  {
+  'imagesrc': 'https://picsum.photos/536/354',
+  'alt': 'some alt',
+  'caption': 'some caption'
+
+  },
+  {
+    'imagesrc': 'https://picsum.photos/id/1025/367/267',
+    'alt': 'some alt2',
+    'caption': 'some caption2'
+  },
+  {
+    'imagesrc': 'https://picsum.photos/id/1024/367/267',
+    'alt': 'some alt3',
+    'caption': 'some caption3'
+  },
+  {
+    'imagesrc': 'https://picsum.photos/id/1023/367/267',
+    'alt': 'some alt4',
+    'caption': 'some caption4'
+
+  },
+  {
+    'imagesrc': 'https://picsum.photos/id/1022/367/267',
+    'alt': 'some alt5',
+    'caption': 'some caption5'
+
+  },
+  {
+    'imagesrc': 'https://picsum.photos/id/1021/367/267',
+    'alt': 'some alt6',
+    'caption': 'some caption6'
+  }
+
+
+
+
+]
